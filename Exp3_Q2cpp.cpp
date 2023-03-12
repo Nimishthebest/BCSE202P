6f854c75176d5f439172d5777aed2b4a310df385
Nimishthebest <65479478+Nimishthebest@users.noreply.github.com>
2023-03-12 21:47:06 +0530

    Update Exp3_Q2cpp.cpp

--- Exp3_Q2cpp.cpp
+++ Exp3_Q2cpp.cpp
@@ -9,28 +9,26 @@ Write a program to calculate mean, variance and standard deviation.
 
 int main()
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
+    int arr[5];
+    for(int i=0;i<5;i++)
     {
         cin>>arr[i];
     }
     int sum=0;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<5;i++)
     {
         sum+=arr[i];
     }
     float mean=sum/n;
     float var=0;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<5;i++)
     {
         var+=pow(arr[i]-mean,2);
     }
-    var=var/n;
+    var=var/5;
     float sd=sqrt(var);
-    cout<<"Mean: "<<mean<<endl;
-    cout<<"Variance: "<<var<<endl;
-    cout<<"Standard Deviation: "<<sd;
+    cout<<mean<<endl;
+    cout<<var<<endl;
+    cout<<sd;
     return 0;
 }
